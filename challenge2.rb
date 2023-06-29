b55503ab3c942a43c6f7576f5e8b674385cca5be
Will Weston <Williamweston81@gmail.com>
2023-06-29 12:39:07 -0500

    Weston(1) variation

--- challenge2.rb
+++ challenge2.rb
@@ -4,7 +4,12 @@ Challenge 2
 Goal: Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
 =end
 
-# Start with array of strings 
+# Start with array of strings of randomly assorted characters in uppercase and lowercase.
 
 random = ["DoG", "BroThEr", "StRINGs", "CoNTORt"]
 
+# array is then iterated to downcase every string in the array
+# puts all strings in downcase format
+random.each do |ran|
+    puts ran.downcase
+end
