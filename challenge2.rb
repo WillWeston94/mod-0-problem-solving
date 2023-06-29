163adf7c6651cbf444fc216a91835b922c1db6a1
Will Weston <Williamweston81@gmail.com>
2023-06-29 12:24:27 -0500

    Weston(1) Challenge2 Create goal

---
+++ challenge2.rb
@@ -0,0 +1,10 @@
+=begin
+Challenge 2
+
+Goal: Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+=end
+
+# Start with array of strings 
+
+random = ["DoG", "BroThEr", "StRINGs", "CoNTORt"]
+
