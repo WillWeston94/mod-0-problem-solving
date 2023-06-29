14683aaa020b268cd05a71b5c19667b2e9843f93
Will Weston <Williamweston81@gmail.com>
2023-06-29 12:30:38 -0500

    Weston(1) Solve challenge 1

---
+++ Challenge1.rb
@@ -0,0 +1,15 @@
+=begin
+Challenge 1
+
+Goal: Given an array of strings, print only the strings that have exactly 4 characters.
+=end
+
+# List out our array full of strings
+words = ["zelda", "backpack", "country", "link"]
+
+# Run iteration through our array to check length of our strings
+# Puts word in length == exactly 4 characters
+
+words.each do |word|
+    p word if word.size == 4
+end
